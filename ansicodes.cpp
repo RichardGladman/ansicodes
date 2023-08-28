7391e77707fe8b53cecbb654cb74a10408b2a940
Richard Gladman <gitlab@thefifthcontinent.com>
2023-08-28 15:21:01 +0100

    Added remaining ansi codes

--- ansicodes.cpp
+++ ansicodes.cpp
@@ -1,4 +1,4 @@
-// <one line to give the program's name and a brief idea of what it does.>
+// ANSI Code constants for terminal programs.
 // SPDX-FileCopyrightText: 2023 Richard Gladman <richard.gladman@thefifthcontinent.com>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
@@ -13,3 +13,49 @@ const std::string ANSICodes::MAGENTA = "\033[35m";
 const std::string ANSICodes::CYAN = "\033[36m";
 const std::string ANSICodes::WHITE = "\033[37m";
 const std::string ANSICodes::DEFAULT_COLOUR = "\033[39m";
+
+const std::string ANSICodes::BLACK_BACKGROUND = "\033[40m";
+const std::string ANSICodes::RED_BACKGROUND = "\033[41m";
+const std::string ANSICodes::GREEN_BACKGROUND = "\033[42m";
+const std::string ANSICodes::YELLOW_BACKGROUND = "\033[43m";
+const std::string ANSICodes::BLUE_BACKGROUND = "\033[44m";
+const std::string ANSICodes::MAGENTA_BACKGROUND = "\033[45m";
+const std::string ANSICodes::CYAN_BACKGROUND = "\033[46m";
+const std::string ANSICodes::WHITE_BACKGROUND = "\033[47m";
+const std::string ANSICodes::DEFAULT_BACKGROUND = "\033[49m";
+
+const std::string ANSICodes::BLACK_BRIGHT = "\033[90m";
+const std::string ANSICodes::RED_BRIGHT = "\033[91m";
+const std::string ANSICodes::GREEN_BRIGHT = "\033[92m";
+const std::string ANSICodes::YELLOW_BRIGHT = "\033[93m";
+const std::string ANSICodes::BLUE_BRIGHT = "\033[94m";
+const std::string ANSICodes::MAGENTA_BRIGHT = "\033[95m";
+const std::string ANSICodes::CYAN_BRIGHT = "\033[96m";
+const std::string ANSICodes::WHITE_BRIGHT = "\033[97m";
+const std::string ANSICodes::DEFAULT_BRIGHT = "\033[99m";
+
+const std::string ANSICodes::BLACK_BACKGROUND_BRIGHT = "\033[100m";
+const std::string ANSICodes::RED_BACKGROUND_BRIGHT = "\033[101m";
+const std::string ANSICodes::GREEN_BACKGROUND_BRIGHT = "\033[102m";
+const std::string ANSICodes::YELLOW_BACKGROUND_BRIGHT = "\033[103m";
+const std::string ANSICodes::BLUE_BACKGROUND_BRIGHT = "\033[104m";
+const std::string ANSICodes::MAGENTA_BACKGROUND_BRIGHT = "\033[105m";
+const std::string ANSICodes::CYAN_BACKGROUND_BRIGHT = "\033[106m";
+const std::string ANSICodes::WHITE_BACKGROUND_BRIGHT = "\033[107m";
+const std::string ANSICodes::DEFAULT_BACKGROUND_BRIGHT = "\033[109m";
+
+const std::string ANSICodes::BOLD = "\033[1m";
+const std::string ANSICodes::BOLD_OFF = "\033[21m";
+const std::string ANSICodes::DIM = "\033[2m";
+const std::string ANSICodes::DIM_OFF = "\033[21m";
+const std::string ANSICodes::UNDERLINE = "\033[4m";
+const std::string ANSICodes::UNDERLINE_OFF = "\033[24m";
+const std::string ANSICodes::BLINK = "\033[5m";
+const std::string ANSICodes::BLINK_OFF = "\033[25m";
+const std::string ANSICodes::REVERSE = "\033[7m";
+const std::string ANSICodes::REVERSE_OFF = "\033[27m";
+const std::string ANSICodes::HIDE = "\033[8m";
+const std::string ANSICodes::HIDE_OFF = "\033[28m";
+
+
+const std::string ANSICodes::RESET = "\033[0m";
--- include/ansicodes.h
+++ include/ansicodes.h
@@ -1,4 +1,4 @@
-// ANSI Code constants for terminal programs.>
+// ANSI Code constants for terminal programs.
 // SPDX-FileCopyrightText: 2023 Richard Gladman <richard.gladman@thefifthcontinent.com>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
@@ -22,6 +22,51 @@ public:
     static const std::string CYAN;
     static const std::string WHITE;
     static const std::string DEFAULT_COLOUR;
+
+    static const std::string BLACK_BACKGROUND;
+    static const std::string RED_BACKGROUND;
+    static const std::string GREEN_BACKGROUND;
+    static const std::string YELLOW_BACKGROUND;
+    static const std::string BLUE_BACKGROUND;
+    static const std::string MAGENTA_BACKGROUND;
+    static const std::string CYAN_BACKGROUND;
+    static const std::string WHITE_BACKGROUND;
+    static const std::string DEFAULT_BACKGROUND;
+
+    static const std::string BLACK_BRIGHT;
+    static const std::string RED_BRIGHT;
+    static const std::string GREEN_BRIGHT;
+    static const std::string YELLOW_BRIGHT;
+    static const std::string BLUE_BRIGHT;
+    static const std::string MAGENTA_BRIGHT;
+    static const std::string CYAN_BRIGHT;
+    static const std::string WHITE_BRIGHT;
+    static const std::string DEFAULT_BRIGHT;
+
+    static const std::string BLACK_BACKGROUND_BRIGHT;
+    static const std::string RED_BACKGROUND_BRIGHT;
+    static const std::string GREEN_BACKGROUND_BRIGHT;
+    static const std::string YELLOW_BACKGROUND_BRIGHT;
+    static const std::string BLUE_BACKGROUND_BRIGHT;
+    static const std::string MAGENTA_BACKGROUND_BRIGHT;
+    static const std::string CYAN_BACKGROUND_BRIGHT;
+    static const std::string WHITE_BACKGROUND_BRIGHT;
+    static const std::string DEFAULT_BACKGROUND_BRIGHT;
+
+    static const std::string BOLD;
+    static const std::string BOLD_OFF;
+    static const std::string DIM;
+    static const std::string DIM_OFF;
+    static const std::string UNDERLINE;
+    static const std::string UNDERLINE_OFF;
+    static const std::string BLINK;
+    static const std::string BLINK_OFF;
+    static const std::string REVERSE;
+    static const std::string REVERSE_OFF;
+    static const std::string HIDE;
+    static const std::string HIDE_OFF;
+
+    static const std::string RESET;
 };
 
 #endif // ANSICODES_H
